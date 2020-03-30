9ea10800ab5e5f5856f65463465662570ee45953
arbiyanto <arbiyantowijaya17@gmail.com>
2020-03-31 04:17:59 +0700

    fix export component

--- projects/crystalui/angular-lightbox/src/public_api.ts
+++ projects/crystalui/angular-lightbox/src/public_api.ts
@@ -6,3 +6,5 @@ export * from './lib/lightbox.module';
 export * from './lib/event.service';
 export * from './lib/lightbox-group.directive';
 export * from './lib/lightbox.directive';
+export * from './lib/lightbox.component';
+export * from './lib/lightbox-common.component';
